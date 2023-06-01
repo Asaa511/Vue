201214411285c87fe9b4738c715c866f9a5dbf09
Asaa511 <115472290+Asaa511@users.noreply.github.com>
2023-06-01 11:54:10 +0800

    Add files via upload

--- my-project/src/routes/index.js
+++ my-project/src/routes/index.js
@@ -1,6 +1,11 @@
 import MyIndex from "@/components/MyIndex.vue";
 import MyUser from "@/components/MyUser.vue";
 import MyCourse from "@/components/MyCourse.vue";
+import JsCourse from "@/components/JsCourse.vue";
+import HtmlCourse from "@/components/HtmlCourse.vue";
+import VueCourse from "@/components/VueCourse.vue";
+import JavaCourse from "@/components/JavaCourse.vue";
+
 export default [
     {
         path:'/index',
@@ -8,10 +13,29 @@ export default [
     },
     {
         path:'/course',
-        component:MyCourse
+        component:MyCourse,
+        children:[
+            {
+                path:"html",
+                component:HtmlCourse
+            },
+            {
+                path:"js",
+                component:JsCourse
+            },
+            {
+                path:"Java",
+                component:JavaCourse
+            },
+            {
+                path:"vue",
+                component:VueCourse
+            },
+        ]
     },
     {
         path:'/user',
         component:MyUser
     },
 ]
+
